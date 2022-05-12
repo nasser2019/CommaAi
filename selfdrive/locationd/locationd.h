12b04e9dcad454d1c1324760d7b41166077ade27
Gijs Koning <gijs-koning@live.nl>
2022-05-12 12:04:05 +0200

    Push locationd and live_kf changes to seperate branch

--- selfdrive/locationd/locationd.h
+++ selfdrive/locationd/locationd.h
@@ -20,6 +20,8 @@
 
 #define POSENET_STD_HIST_HALF 20
 
+typedef cereal::GnssMeasurements::ConstellationId ConstellationId;
+
 class Localizer {
 public:
   Localizer();
@@ -27,7 +29,8 @@ public:
   int locationd_thread();
 
   void reset_kalman(double current_time = NAN);
-  void reset_kalman(double current_time, Eigen::VectorXd init_orient, Eigen::VectorXd init_pos, Eigen::VectorXd init_vel, MatrixXdr init_pos_R, MatrixXdr init_vel_R);
+  // void reset_kalman(double current_time, Eigen::VectorXd init_pos, Eigen::VectorXd init_vel, MatrixXdr init_pos_R, MatrixXdr init_vel_R);
+  void reset_kalman(double current_time, Eigen::VectorXd init_pos, Eigen::VectorXd init_vel, MatrixXdr init_pos_R, MatrixXdr init_vel_R, Eigen::VectorXd* init_orient=nullptr);
   void reset_kalman(double current_time, Eigen::VectorXd init_x, MatrixXdr init_P);
   void finite_check(double current_time = NAN);
   void time_check(double current_time = NAN);
@@ -46,6 +49,7 @@ public:
   void handle_msg_bytes(const char *data, const size_t size);
   void handle_msg(const cereal::Event::Reader& log);
   void handle_sensors(double current_time, const capnp::List<cereal::SensorEventData, capnp::Kind::STRUCT>::Reader& log);
+  void handle_gnss_measurements(double current_time, const cereal::GnssMeasurements::Reader& log);
   void handle_gps(double current_time, const cereal::GpsLocationData::Reader& log);
   void handle_car_state(double current_time, const cereal::CarState::Reader& log);
   void handle_cam_odo(double current_time, const cereal::CameraOdometry::Reader& log);
@@ -72,4 +76,7 @@ private:
   double reset_tracker = 0.0;
   bool device_fell = false;
   bool gps_mode = false;
+
+  void handle_gnss_constellation(double current_time, ConstellationId c_id, std::vector<cereal::GnssMeasurements::CorrectedMeasurement::Reader> meas_per_constellation);
+  void check_initial_position(double time, const cereal::GnssMeasurements::Reader& log);
 };
