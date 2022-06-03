06a5b2d151aa39b8fc4d6e8df37f753b10c83664
Gijs Koning <gijs-koning@live.nl>
2022-06-03 21:06:22 +0200

    Revert some changes

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -56,7 +56,6 @@ procs = [
 
   # Experimental
   PythonProcess("rawgpsd", "selfdrive.sensord.rawgps.rawgpsd", enabled=os.path.isfile("/persist/comma/use-quectel-rawgps")),
-  PythonProcess("laikad", "selfdrive.locationd.laikad"),
 ]
 
 managed_processes = {p.name: p for p in procs}
